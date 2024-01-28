5000a0f0170c53e9e6515210461e4c2f23258a53
Danish <rjcdanishsiddique11sci534@gmail.com>
2024-01-28 10:34:16 +0530

    compliting java programming

--- factorial.java
+++ factorial.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class factorial {
 
 
-    public static int fact(int n){
+    public static int fact(double n){
+
         int f=1;
         for(int i = 1; i <= n; i++) {
             f= f*i;
@@ -13,7 +14,10 @@ public class factorial {
 
     }
     public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)){
+            double n = sc.nextInt();
+            System.out.println(fact(n));
+        }
         
-        System.out.println(fact(7));
     }
 }
