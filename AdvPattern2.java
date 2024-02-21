17cd47e0df86eafab99c60a74e8d04b58f32b5c8
Danish <rjcdanishsiddique11sci534@gmail.com>
2024-02-21 12:19:31 +0530

    done

--- AdvPattern2.java
+++ AdvPattern2.java
@@ -66,11 +66,50 @@ public class AdvPattern2 {
         }
     }
 
+    public static void butterfly(int n) {
+        // upper half
+        
+        for (int i = 1; i <= n; i++) {
+            // for star - i
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // space - 2*(n-i)
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            // for star - i
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+        // lower half
+     
+        for (int i = n; i >= 1; i--) {
+            // for star - i
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // space - 2*(n-i)
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            // for star - i
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         // half_priamid(5);
         // invertPrimidPttrnNumb(85);
         // Floyid_tringle(5);
         // hollow_pattern(4, 4);
-        zero_one_tringle(5);
+        // zero_one_tringle(5);
+        butterfly(4);
     }
 }
